3505c6a8c05dab619275e2bd3efb9da86870b0f8
Jan Van Hoye <janvanhoye@MacBook-Pro.localdomain>
2020-02-29 18:05:32 +0100

    hamburger menu and main page responsive init

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable global-require */
 import React, { useState } from 'react';
 import { hot } from 'react-hot-loader/root';
@@ -5,6 +7,7 @@ import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
 import NotFound from './pages/NotFound';
 import Main from './pages/Main';
 import logo from './public/images/LOGO-OLKARIA.png';
+import hamburger from './public/images/hamburger.svg';
 
 export function App() {
   const [ham, setHam] = useState(false);
@@ -45,14 +48,41 @@ export function App() {
               </NavLink>
             </div>
           </div>
-          <button type="button" className="nav-ham" onClick={() => setHam(prev => !prev)}>
-            ham
-          </button>
+          <img src={hamburger} alt="hamburger" className="nav-ham" onClick={() => setHam(prev => !prev)} />
         </div>
         <div className={`nav-sidebar ${ham && 'active'}`}>
-          <button type="button" className="nav-sidebar-close" onClick={() => setHam(prev => !prev)}>
-            X
-          </button>
+          <div className="nav-sidebar-container">
+            <div className="nav-button">
+              <NavLink exact activeClassName="selected" to="/" onClick={() => setHam(prev => !prev)}>
+                Holidays
+              </NavLink>
+            </div>
+            <div className="nav-button">
+              <NavLink exact activeClassName="selected" to="/kite" onClick={() => setHam(prev => !prev)}>
+                Kitesurfing
+              </NavLink>
+            </div>
+            <div className="nav-button">
+              <NavLink exact activeClassName="selected" to="/offer" onClick={() => setHam(prev => !prev)}>
+                Offers
+              </NavLink>
+            </div>
+            <div className="nav-button">
+              <NavLink exact activeClassName="selected" to="/shop" onClick={() => setHam(prev => !prev)}>
+                Shop
+              </NavLink>
+            </div>
+            <div className="nav-button">
+              <NavLink exact activeClassName="selected" to="/contact" onClick={() => setHam(prev => !prev)}>
+                Contact
+              </NavLink>
+            </div>
+            <div className="nav-button">
+              <NavLink exact activeClassName="selected" to="/about" onClick={() => setHam(prev => !prev)}>
+                About
+              </NavLink>
+            </div>
+          </div>
         </div>
 
         <div className="Container">
